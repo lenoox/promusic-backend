66e1b04b73946645b37a1cb2a04219ddcbba2285
lenoox <62141973+lenoox@users.noreply.github.com>
2021-04-09 20:50:28 +0200

    fixed problem with dupclicate testdata

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -1,8 +1,9 @@
-INSERT INTO public.brands (brand_name) VALUES ('TSPV') ON CONFLICT DO NOTHING;
-INSERT INTO public.brands (brand_name) VALUES ('DHQForMusic') ON CONFLICT DO NOTHING;
-INSERT INTO public.brands (brand_name) VALUES ('UFreshSound') ON CONFLICT DO NOTHING;
-INSERT INTO public.brands (brand_name) VALUES ('PRTU-X') ON CONFLICT DO NOTHING;
-INSERT INTO public.brands (brand_name) VALUES ('YourHighSoundXK') ON CONFLICT DO NOTHING;
-INSERT INTO public.order_status (status_name) VALUES ('Nowy') ON CONFLICT DO NOTHING;
-INSERT INTO public.order_status (status_name) VALUES ('Opłacony') ON CONFLICT DO NOTHING;
-INSERT INTO public.order_status (status_name) VALUES ('Wysłany') ON CONFLICT DO NOTHING;
+INSERT INTO public.brands (brand_id,brand_name) VALUES (1,'TSPV') ON CONFLICT DO NOTHING;
+INSERT INTO public.brands (brand_id,brand_name) VALUES (2,'DHQForMusic') ON CONFLICT DO NOTHING;
+INSERT INTO public.brands (brand_id,brand_name) VALUES (3,'PRTU-X') ON CONFLICT DO NOTHING;
+INSERT INTO public.brands (brand_id,brand_name) VALUES (4,'YourHighSoundXK') ON CONFLICT DO NOTHING;
+SELECT setval('brands_brand_id_seq', 5);
+INSERT INTO public.order_status (order_status_id, status_name) VALUES (1,'Nowy') ON CONFLICT DO NOTHING;
+INSERT INTO public.order_status (order_status_id, status_name) VALUES (2,'Opłacony') ON CONFLICT DO NOTHING;
+INSERT INTO public.order_status (order_status_id, status_name) VALUES (3,'Wysłany') ON CONFLICT DO NOTHING;
+SELECT setval('order_status_order_status_id_seq', 4);
