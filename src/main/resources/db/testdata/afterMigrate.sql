caae5203f51d274271ab78bbf63ab9d8f5589ec8
lenoox <62141973+lenoox@users.noreply.github.com>
2021-04-09 21:00:09 +0200

    fixed manually increment id in testing data

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -2,8 +2,8 @@ INSERT INTO public.brands (brand_id,brand_name) VALUES (1,'TSPV') ON CONFLICT DO
 INSERT INTO public.brands (brand_id,brand_name) VALUES (2,'DHQForMusic') ON CONFLICT DO NOTHING;
 INSERT INTO public.brands (brand_id,brand_name) VALUES (3,'PRTU-X') ON CONFLICT DO NOTHING;
 INSERT INTO public.brands (brand_id,brand_name) VALUES (4,'YourHighSoundXK') ON CONFLICT DO NOTHING;
-SELECT setval('brands_brand_id_seq', 5);
+SELECT setval('brands_brand_id_seq', 4);
 INSERT INTO public.order_status (order_status_id, status_name) VALUES (1,'Nowy') ON CONFLICT DO NOTHING;
 INSERT INTO public.order_status (order_status_id, status_name) VALUES (2,'Opłacony') ON CONFLICT DO NOTHING;
 INSERT INTO public.order_status (order_status_id, status_name) VALUES (3,'Wysłany') ON CONFLICT DO NOTHING;
-SELECT setval('order_status_order_status_id_seq', 4);
+SELECT setval('order_status_order_status_id_seq', 3);
